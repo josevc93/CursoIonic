8e6be2e34747d5f5b9189d3915f5b6c4ca35f567
josevc93 <josemanuel.vazquezcrespo@gmail.com>
2018-03-14 17:50:59 +0100

    last day

--- bookApp2/src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ExpandableContentListComponent } from './expandable-content-list/expandable-content-list';
-@NgModule({
-	declarations: [ExpandableContentListComponent],
-	imports: [],
-	exports: [ExpandableContentListComponent]
-})
-export class ComponentsModule {}
